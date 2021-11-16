e8aaff6e3fbc4e215cb77de8e17856f6e8d60ad5
tuanlinhtony <mrpi3990@gmail.com>
2021-11-16 19:09:31 +0800

    Importing npm modules and printing in color

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,6 +1,15 @@
 const fs = require("fs");
 const getNotes = require("./utils.js");
+const validator = require("validator");
+const chalk = require("chalk");
 //fs.writeFileSync('notes.txt', "this file was created by Node.js");
 //fs.appendFileSync('notes.txt', " test appendFilesync");
 // fs.copyFile("notes.txt", ["./notes-app", false]);
-console.log(getNotes)
+// console.log(getNotes)
+if(validator.isEmail("test")){
+    console.log(chalk.blueBright("Yes"));
+}
+else{
+    console.log(chalk.redBright("No"));
+}
+    
