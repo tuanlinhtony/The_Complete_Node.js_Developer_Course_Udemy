349576d28d789ce08a1cab8d8b8235a7229a95d0
tuanlinhtony <mrpi3990@gmail.com>
2021-11-16 19:36:05 +0800

    global npm modules and nodemon

--- notes_app/app.js
+++ notes_app/app.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const getNotes = require("./utils.js");
 const validator = require("validator");
 const chalk = require("chalk");
+const nodemon = require("nodemon")
 //fs.writeFileSync('notes.txt', "this file was created by Node.js");
 //fs.appendFileSync('notes.txt', " test appendFilesync");
 // fs.copyFile("notes.txt", ["./notes-app", false]);
@@ -10,6 +11,13 @@ if(validator.isEmail("test")){
     console.log(chalk.blueBright("Yes"));
 }
 else{
-    console.log(chalk.redBright("No"));
+    console.log(chalk.yellow("No"));
 }
     
+//Currently, I am using nodemon module, I had have the issue when I only install it with no global
+//the error I received is:
+// nodemon : The term 'nodemon' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the 
+// spelling of the name, or if a path was included, verify that the path is correct and try again.
+// At line:1 char:1
+// + nodemon -v
+//     + CategoryInfo          : ObjectNotFound: (nodemon:String) [], CommandNotFoundException
