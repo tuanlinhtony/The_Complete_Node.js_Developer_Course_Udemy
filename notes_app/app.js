a32e56bd344ae2f3dc010439c8b82c4e4df9f4f2
tuanlinhtony <mrpi3990@gmail.com>
2021-11-16 17:43:28 +0800

    Importing my own files

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const getNotes = require("./utils.js");
 //fs.writeFileSync('notes.txt', "this file was created by Node.js");
 //fs.appendFileSync('notes.txt', " test appendFilesync");
-fs.copyFile("notes.txt", ["./notes-app", false]);
+// fs.copyFile("notes.txt", ["./notes-app", false]);
+console.log(getNotes)
