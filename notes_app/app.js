15e4e23f542d83cb5af1b1ba810446665ab49d60
tuanlinhtony <mrpi3990@gmail.com>
2021-11-20 19:56:00 +0800

    Argument Parsing with Yargs

--- notes_app/app.js
+++ notes_app/app.js
@@ -2,17 +2,19 @@ const fs = require("fs");
 const getNotes = require("./utils.js");
 const validator = require("validator");
 const chalk = require("chalk");
-const nodemon = require("nodemon")
+const nodemon = require("nodemon");
+const yargs = require("yargs");
+
 //fs.writeFileSync('notes.txt', "this file was created by Node.js");
 //fs.appendFileSync('notes.txt', " test appendFilesync");
 // fs.copyFile("notes.txt", ["./notes-app", false]);
 // console.log(getNotes)
-if(validator.isEmail("test")){
-    console.log(chalk.blueBright("Yes"));
-}
-else{
-    console.log(chalk.yellow("No"));
-}
+// if(validator.isEmail("test")){
+//     console.log(chalk.blueBright("Yes"));
+// }
+// else{
+//     console.log(chalk.yellow("No"));
+// }
     
 //Currently, I am using nodemon module, I had have the issue when I only install it with no global
 //the error I received is:
@@ -21,3 +23,54 @@ else{
 // At line:1 char:1
 // + nodemon -v
 //     + CategoryInfo          : ObjectNotFound: (nodemon:String) [], CommandNotFoundException
+// console.log(process.argv);
+
+
+
+//Customize yargs version
+yargs.version('1.1.0')
+
+
+yargs.command(
+    {
+        command: 'add',
+        describe: 'Add a new note',
+        handler: function(){
+            console.log("Adding a new note!!");
+        }
+    }
+)
+
+//Creat remove command
+yargs.command(
+    {
+        command:'remove',
+        describe:'Remove a note',
+        handler: function(){
+            console.log("Removing the note")
+        }
+    }
+)
+//add, remove, read, list
+
+
+//Challenge:
+yargs.command(
+    {
+        command:'list',
+        describe: "List all your notes",
+        handler: function(){
+            console.log("List your notes")
+        }
+    }
+)
+yargs.command(
+    {
+        command:'read',
+        describe: "Read your notes",
+        handler: function(){
+            console.log("Read your notes")
+        }
+    }
+)
+console.log(yargs.argv);
