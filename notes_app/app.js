e6455d30c45e7b06aee2f3a786f11bc98ed29aeb
tuanlinhtony <mrpi3990@gmail.com>
2021-11-24 14:33:46 +0800

    Adding a Note

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,73 +1,14 @@
-const fs = require("fs");
-const getNotes = require("./utils.js");
-const validator = require("validator");
-const chalk = require("chalk");
-const nodemon = require("nodemon");
-const yargs = require("yargs");
-
-//fs.writeFileSync('notes.txt', "this file was created by Node.js");
-//fs.appendFileSync('notes.txt', " test appendFilesync");
-// fs.copyFile("notes.txt", ["./notes-app", false]);
-// console.log(getNotes)
-// if(validator.isEmail("test")){
-//     console.log(chalk.blueBright("Yes"));
-// }
-// else{
-//     console.log(chalk.yellow("No"));
-// }
-    
-//Currently, I am using nodemon module, I had have the issue when I only install it with no global
-//the error I received is:
-// nodemon : The term 'nodemon' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the 
-// spelling of the name, or if a path was included, verify that the path is correct and try again.
-// At line:1 char:1
-// + nodemon -v
-//     + CategoryInfo          : ObjectNotFound: (nodemon:String) [], CommandNotFoundException
-// console.log(process.argv);
-
-
-
-//Customize yargs version
-yargs.version('1.1.0')
-
+const fs = require("fs")
+const notes = require("./utils.js")
+const validator = require("validator")
+const chalk = require("chalk")
+const nodemon = require("nodemon")
+const yargs = require("yargs")
 
 yargs.command(
     {
-        command: 'add',
-        describe: 'Add a new note',
-        handler: function(argv){
-            console.log("Adding a new note!!", argv);
-        }
-    }
-)
-
-//Creat remove command
-yargs.command(
-    {
-        command:'remove',
-        describe:'Remove a note',
-        handler: function(){
-            console.log("Removing the note")
-        }
-    }
-)
-//add, remove, read, list
-
-
-//Challenge:
-yargs.command(
-    {
-        command:'list',
-        describe: "List all your notes",
-        handler: function(){
-            console.log("List your notes")
-        }
-    }
-)
-yargs.command(
-    {
-        command:'read',
-        describe: "Read your notes",
+        command:'add',
+        describe: "Add new notes",
         builder:{
             title:{
                 describe:'Note title details',
@@ -81,10 +22,8 @@ yargs.command(
             }
         },
         handler: function(argv){
-            console.log("Tittle" + argv.title)
-            console.log("Body" + argv.body)
+            notes.addNote(argv.title, argv.body)
         }
     }
 )
-//console.log(yargs.argv);
 yargs.parse()
--- notes_app/utils.js
+++ notes_app/utils.js
@@ -1,7 +1,46 @@
+const fs = require('fs')
+
+//create function getNotes
 const getNotes = function(){
     return 'Your notes...'
 }
+//create function addnote
+const addNote = function(title, body){
+    const notes = loadNotes() //call loadNotes function
+    const duplicateNotes = notes.filter(function(notes){
+        return notes.title === title
+    })
 
-module.exports = getNotes();
-// last time, my mistake was no write symboy () after variable getNotes so I will not receive return of 
-// getNotes function
+    if(duplicateNotes.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        });
+        saveNotes(notes)
+        console.log('New note added')
+    }
+    else{
+        console.log('Note title taken')
+    }
+    
+}
+
+const saveNotes = function(notes){
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function(){
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (error) {
+        return []
+    }
+}
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
+
