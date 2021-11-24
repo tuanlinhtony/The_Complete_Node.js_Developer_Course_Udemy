a1a175924734632497e351993b99bea64f651edd
tuanlinhtony <mrpi3990@gmail.com>
2021-11-24 14:46:05 +0800

    Adding a Note (more comment code)

--- notes_app/utils.js
+++ notes_app/utils.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
-
+// This lesson helps me to create the command to add something in server side like add new user, new account etc. 
+// I thought so. This spent 5 times rewatch this lesson, currently I got it. so great!!! I feel so happy that 
+// I had understanded it finally. 
 //create function getNotes
 const getNotes = function(){
     return 'Your notes...'
@@ -7,16 +9,17 @@ const getNotes = function(){
 //create function addnote
 const addNote = function(title, body){
     const notes = loadNotes() //call loadNotes function
+    //the varriable to check duplication notes.title
     const duplicateNotes = notes.filter(function(notes){
         return notes.title === title
     })
-
+    // the condition allows write this note to notes.json
     if(duplicateNotes.length === 0){
         notes.push({
             title: title,
             body: body
         });
-        saveNotes(notes)
+        saveNotes(notes) //call saveNotes function below
         console.log('New note added')
     }
     else{
