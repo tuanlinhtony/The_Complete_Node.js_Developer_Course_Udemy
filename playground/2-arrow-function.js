8f293fe10bf7dfc2ec73f39a2a03e5870566a65f
tuanlinhtony <mrpi3990@gmail.com>
2021-11-25 13:51:35 +0800

    Arrow function with ES6

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -6,5 +6,22 @@
 //     return x * x
 // }
 
-const square = (x) => x * x
-console.log(square(5))
+// const square = (x) => x * x
+// console.log(square(5))
+
+const event = {
+    name: "Birthday Party",
+    guestList: ['Andrew', 'Jen', 'Mike'],
+    printGuestList() {
+        // console.log('Guest list for ' + this.name)
+        // this.guestList.forEach(function(guest){
+        //     console.log(guest + ' is attending ' + this.name)
+        // })
+        const that = this
+        console.log('Guest list for ' + this.name)
+        this.guestList.forEach((guest) =>{
+            console.log(guest + ' is attending ' + this.name)
+        })
+    }
+}
+event.printGuestList()
