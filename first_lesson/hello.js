52b7b23aaa14b6f13f222890cc836434244e4104
tuanlinhtony <mrpi3990@gmail.com>
2021-11-29 14:58:42 +0800

    Asynchronous Basics

---
+++ first_lesson/hello.js
@@ -0,0 +1,3 @@
+// using nodejs ver v16.13.0 LTS
+// console.log("I am learning Nodejs");
+//run node hello.js => I am learning Nodejs
---
+++ weather-app/app.js
@@ -0,0 +1,11 @@
+// Asynchronous Basics
+console.log('Starting')
+
+setTimeout(() => {
+    console.log('Two Second Timer')
+}, 2000)
+
+setTimeout(() => {
+    console.log('Zero Second Timer')
+}, 0)
+console.log('Stopping')
