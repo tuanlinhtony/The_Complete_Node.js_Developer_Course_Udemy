47d415eeb001398b300ec7b36f64128732d12699
tuanlinhtony <mrpi3990@gmail.com>
2021-11-29 17:46:55 +0800

    Making HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,19 @@
-// Asynchronous Basics
-console.log('Starting')
+// // Asynchronous Basics
+// console.log('Starting')
 
-setTimeout(() => {
-    console.log('Two Second Timer')
-}, 2000)
+// setTimeout(() => {
+//     console.log('Two Second Timer')
+// }, 2000)
 
-setTimeout(() => {
-    console.log('Zero Second Timer')
-}, 0)
-console.log('Stopping')
+// setTimeout(() => {
+//     console.log('Zero Second Timer')
+// }, 0)
+// console.log('Stopping')
+
+const request = require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=2c36b2e7e1d2846c6958befb90181c83&query=37.8267,-122.423'
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data)
+})
