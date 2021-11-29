bb9acb9e7e728a6a32ff0ae8b24eae45728c8cc0
tuanlinhtony <mrpi3990@gmail.com>
2021-11-29 18:32:58 +0800

    Customizing HTTP request

--- weather-app/app.js
+++ weather-app/app.js
@@ -12,8 +12,9 @@
 
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=2c36b2e7e1d2846c6958befb90181c83&query=37.8267,-122.423'
+const url = 'http://api.weatherstack.com/current?access_key=2c36b2e7e1d2846c6958befb90181c83&query=38.7267,-122.423&units=f'
 request({ url: url }, (error, response) => {
     const data = JSON.parse(response.body)
-    console.log(data)
+    console.log(data.current.weather_descriptions + " .It is currently " + data.current.temperature 
+    + " degress out. There is a " + data.current.precip + "% chance of rain")    
 })
