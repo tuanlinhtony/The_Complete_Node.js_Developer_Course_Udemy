ea8e1f8ef55892cac066683fba166efc730f94c8
tuanlinhtony <mrpi3990@gmail.com>
2021-11-30 16:40:12 +0800

    Callback Abstraction - Try callback with geocoding mapbox

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -18,16 +18,34 @@ setTimeout(() => {
 // const data = geocode("New York")
 // console.log(data)
 
-const geocode = (address, callback) =>{
+// const geocode = (address, callback) =>{
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
+//         callback(data)
+//     }, 2000)
+// }
+
+// geocode("New York", (data) => {
+//     console.log(data)
+// })
+
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+const add = (a, b, callback) =>{
     setTimeout(() => {
-        const data = {
-            latitude: 0,
-            longitude: 0
-        }
-        callback(data)
+        const sum = a + b
+        callback(sum)
     }, 2000)
 }
-
-geocode("New York", (data) => {
-    console.log(data)
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
 })
