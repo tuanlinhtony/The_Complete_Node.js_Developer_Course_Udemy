2c0086023a10b4bccf1f99943aa370a2c6979797
tuanlinhtony <mrpi3990@gmail.com>
2021-12-13 21:27:10 +0800

    Advanced Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,24 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
-
+hbs.registerPartials(partialsPath)
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather App',
+        title: 'Weather',
         name: 'Tony'
     })
 })
@@ -29,17 +31,10 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
     res.render('help', {
+        title : 'HELP PAGE',
         helpText: 'This is some helpful text'
     })
 })
-
-
-// app.get("/weather", (req, res) => {
-//     res.send({
-//         forecast: 'It is snowing',
-//         location: 'Philadelphia'
-//     })
-// })
 
 app.listen(3000, () => {
     console.log("Server is up on port 3000")
