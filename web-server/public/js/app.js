0eef1b05333dea1d04851efc913eca802ace0b1a
tuanlinhtony <mrpi3990@gmail.com>
2021-12-15 16:18:54 +0800

    Wiring up the User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -13,14 +13,34 @@ console.log("Client side javascript is loaded")
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
+const messageOne = document.querySelector('#msg-01')
+const messageTwo = document.querySelector('#msg-02')
+const messageThree = document.querySelector('#msg-03')
+
+
+
 weatherForm.addEventListener('submit', (e) =>{
+    messageOne.textContent="Loading ..."
+    messageTwo.textContent = ''
+    messageThree.textContent = ''
     e.preventDefault()
     const location = search.value
+    
     fetch("http://localhost:3000/weather?address=" + location).then((response) =>{
         console.log(location)
         response.json().then((data) =>{
-            console.log(data)
+            if(data.error){
+                messageOne.textContent = data.error
+
+            }else{
+                console.log(data)
+                messageOne.textContent= "Location: " + data.location,
+                messageTwo.textContent= "Date: " + data.date,
+                messageThree.textContent= "Tempature: " + data.forecastTemp
+            }
+            
         })
     })
     console.log('get weather')
 })
+
