3df3a3dd4f7726467efbc8bc866f4c96a94152ec
tuanlinhtony <mrpi3990@gmail.com>
2021-12-20 17:57:46 +0800

    Create Mongoose module

--- task-manager/src/mongoose.js
+++ task-manager/src/mongoose.js
@@ -5,22 +5,44 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useUnifiedTopology: true 
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
+// Create a model for tasks
+const Task = mongoose.model('Tasks', {
+    description: {
+        type: String
     },
-    age : {
-        type : Number
+    completed:{
+        type: Boolean
     }
- })
-
-const me = new User({
-    name: 'Tony',
-    age: 31
 })
 
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
+const task = new Task({
+    description: 'Clean my house',
+    completed: false
 })
+
+task.save().then((result) => {
+    console.log(result)
+}).catch((error) =>{
+    console.log(error)
+})
+
+// // Insert document with Mongoose
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//     },
+//     age : {
+//         type : Number
+//     }
+//  })
+
+// const me = new User({
+//     name: 'Tony',
+//     age: 31
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
