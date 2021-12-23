ed41da37eb8cb2fa850e053ddc7be4fd85d06b85
tuanlinhtony <mrpi3990@gmail.com>
2021-12-23 21:03:37 +0800

    Resource Reading Endpoints 2

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,16 +9,41 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 // Create api for add new user
-app.post('/user', (req,res) => {
+app.post('/users', (req,res) => {
     const user = new User(req.body)
-    user.save().then((result) => {
+    user.save().then((user) => {
         res.send(user)
     }).catch((error) => {
         res.status(400).send(error)
     })
 })
 
-app.post('/task', (req,res) => {
+// Create api for find all users
+app.get('/users', (req,res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+// Create api for find user with ID
+app.get('/users/:id', (req,res) => {
+    console.log(req.params)
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) =>{
+        res.status(500).send()
+    })
+})
+
+// Create api for create new task
+app.post('/tasks', (req,res) => {
     const task = new Task(req.body)
     task.save().then((result) => {
         res.status(201).send(task)
