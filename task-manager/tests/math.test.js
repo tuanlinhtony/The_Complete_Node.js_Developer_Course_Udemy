ff6430a0e4364da91df03118cff8ea09d43eb50d
linhvu3990 <mrpi3990@gmail.com>
2022-01-12 12:45:41 +0800

    Jest Testing Framework

---
+++ task-manager/tests/math.test.js
@@ -0,0 +1,14 @@
+const {calculateTip} = require('../src/math')
+
+
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+
+    if(total !== 13){
+        throw new Error('Total tip should be 13. Got ' + total)
+    }
+})
+
+test('This is the failure!', () => {
+    throw new Error('Failure')
+})
