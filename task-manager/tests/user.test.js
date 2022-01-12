56a6bfa276527201de8b6d413f43fef880866641
linhvu3990 <mrpi3990@gmail.com>
2022-01-12 22:17:57 +0800

    Testing with Authencation

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,17 +1,26 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
-const userOne={
+const userOneId = new mongoose.Types.ObjectId() 
+const userOne = {
+    _id: userOneId,
     name: 'Mike',
     email :  "tuanlinh3667@gmail.com",
     age : "33",
-    password: "test123456"
+    password: "test123456",
+    tokens :[{
+        token: jwt.sign({_id:userOneId}, process.env.JWT_SECRET)
+    }]
 }
 beforeEach(async () => {
     await User.deleteMany()
     await new User(userOne).save()
 })
+
+//Test register a new user
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
         name: "Alena",
@@ -21,6 +30,7 @@ test('Should signup a new user', async () => {
     }).expect(201)
 })
 
+//Test login
 test('Should login existing user', async () => {
     await ( request(app).post('/users/login')).send({
         email :  userOne.email,
@@ -28,9 +38,45 @@ test('Should login existing user', async () => {
     })
 })
 
+//Test failure login if wrong authencation
 test('Should login nonexisting user', async () => {
     await ( request(app).post('/users/login')).send({
         email :  userOne.email,
         password: 'thisisnotpassword'
     }).expect(400)
+}) 
+
+console.log('Bearer ' + userOne.tokens[0].token)
+// Test show profile of existing user
+test('Should get profile for user', async () =>{
+    await request(app)
+    .get('/users/me')
+    .set('Authorization', 'Bearer ' + userOne.tokens[0].token)
+    .send()
+    .expect(200)
+})
+
+//Test no show profile with wrong authencation
+test('Should get profile for user', async () =>{
+    await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401)
+})
+
+//Test delete account
+test('Should delete account for user', async () =>{
+    await request(app)
+    .delete('/users/me')
+    .set('Authorization', 'Bearer ' + userOne.tokens[0].token)
+    .send()
+    .expect(200)
+})
+
+//Test not allow to delete account if wrong authencation
+test('Should delete account for user', async () =>{
+    await request(app)
+    .delete('/users/me')
+    .send()
+    .expect(401)
 })
