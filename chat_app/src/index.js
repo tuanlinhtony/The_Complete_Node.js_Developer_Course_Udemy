e8788d49bf58200a0e2035c92b8b32a92edf410e
linhvu3990 <mrpi3990@gmail.com>
2022-01-16 18:03:02 +0800

    Broadcasting Events

--- chat_app/src/index.js
+++ chat_app/src/index.js
@@ -16,10 +16,14 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
     socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
     socket.on('sendMessage', (message) =>{
         io.emit('message', message)
     })
 
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
 
     // socket.emit('countUpdated', count)
     // socket.on('increment', ()=>{
