e54c0cb905632cd1820a5dd723a5a12d82dd1be3
linhvu3990 <mrpi3990@gmail.com>
2022-01-16 15:41:53 +0800

    Getting started with socket.io

---
+++ chat_app/public/js/chat.js
@@ -0,0 +1,9 @@
+const socket = io()
+socket.on('countUpdated', (count) => {
+    console.log('the count has been updated', count)
+})
+
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('Clicked')
+    socket.emit('increment')
+})
