75c29b9e206095a042c8944b9d08316d10a46627
linhvu3990 <mrpi3990@gmail.com>
2022-01-16 16:48:08 +0800

    Create form chat realtiem with Socket.io

--- chat_app/public/js/chat.js
+++ chat_app/public/js/chat.js
@@ -1,9 +1,20 @@
 const socket = io()
-socket.on('countUpdated', (count) => {
-    console.log('the count has been updated', count)
+socket.on('message', (message) => {
+    console.log(message)
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked')
-    socket.emit('increment')
+document.querySelector('#message-form').addEventListener('submit', (e) =>{
+    e.preventDefault()
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message)
 })
+
+
+// socket.on('countUpdated', (count) => {
+//     console.log('the count has been updated', count)
+// })
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('Clicked')
+//     socket.emit('increment')
+// })
--- chat_app/src/index.js
+++ chat_app/src/index.js
@@ -12,16 +12,21 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
+// let count = 0
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
-    socket.emit('countUpdated', count)
-
-    socket.on('increment', ()=>{
-        count++
-        // socket.emit('countUpdated', count)
-        io.emit('countUpdated', count)
+    socket.emit('message', 'Welcome!')
+    socket.on('sendMessage', (message) =>{
+        io.emit('message', message)
     })
+
+
+    // socket.emit('countUpdated', count)
+    // socket.on('increment', ()=>{
+    //     count++
+    //     // socket.emit('countUpdated', count)
+    //     io.emit('countUpdated', count)
+    // })
 })
 
 server.listen(port, () => {
