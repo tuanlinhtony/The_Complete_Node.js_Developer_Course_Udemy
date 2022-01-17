c55bc7c7bd84774b3232e66c2ccd52d12bc44f36
linhvu3990 <mrpi3990@gmail.com>
2022-01-17 15:18:02 +0800

    Event Acknowlegement

--- chat_app/src/index.js
+++ chat_app/src/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
-
+const Filter = require('bad-words')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
@@ -18,12 +18,20 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!')
 
     socket.broadcast.emit('message', 'A new user has joined!')
-    socket.on('sendMessage', (message) =>{
+    socket.on('sendMessage', (message, callback) =>{
+        const filter = new Filter()
+
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allow!')
+        }
+
         io.emit('message', message)
+        callback()
     })
 
-    socket.on('sendLocation', (position) => {
+    socket.on('sendLocation', (position, callback) => {
         io.emit('message', 'https://google.com/maps?q=' + position.latitude + ',' + position.longitude)
+        callback()
     })
 
     socket.on('disconnect', () => {
